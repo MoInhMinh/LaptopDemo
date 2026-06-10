aa1afdf30e378c6f6521e74ea0f040aba784927b
X1C.memberA <X1C.memberA@email>
2026-06-10 22:31:06 +0700

    Add file SoundSpeaker.java

---
+++ src/mainPackage/SoundSpeaker.java
@@ -0,0 +1,12 @@
+package mainPackage;
+
+public class SoundSpeaker {
+
+	public void volumeUp() {
+		System.out.println("Turning volume up...");
+	}
+
+	public void volumeDown() {
+		System.out.println("Turning volume down...");
+	}
+}
