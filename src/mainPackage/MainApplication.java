b419e3e226c8448b05c9392d699e21689ccf5076
Minh Do Nhat <MinhDN22@fpt.com>
2026-06-10 17:39:18 +0700

    Add feature: 'zoom'.

--- src/mainPackage/MainApplication.java
+++ src/mainPackage/MainApplication.java
@@ -1,11 +1,20 @@
 package mainPackage;
 
+import java.util.Scanner;
+
 public class MainApplication {
 
 	public static void main(String[] args) {
 
 		Laptop laptop = new Laptop();
 		laptop.camera.takePhoto();
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the laptop zoomLevel:");
+		laptop.camera.setZoomLevel(scanner.nextDouble());
+		System.out.println("Current zoom level: " + laptop.camera.getZoomLevel());
+
+		scanner.close();
 
 	}
 
