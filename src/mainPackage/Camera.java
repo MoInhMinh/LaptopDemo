1f2e5c6f99c166eb9e9e9f0b57875b87dfbd9001
Minh Do Nhat <MinhDN22@fpt.com>
2026-06-10 17:39:18 +0700

    Add feature: 'zoom'.

--- src/mainPackage/Camera.java
+++ src/mainPackage/Camera.java
@@ -1,7 +1,18 @@
 package mainPackage;
 
 public class Camera {
+	private double zoomLevel;
+
 	public void takePhoto() {
 		System.out.println("Photo taken!");
 	}
+
+	public double getZoomLevel() {
+		return zoomLevel;
+	}
+
+	public void setZoomLevel(double zoomLevel) {
+		this.zoomLevel = zoomLevel;
+	}
+
 }
